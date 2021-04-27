9ef8984edd01b6d5d3e987ca096ee632e06c171e
Leon Li <leonli369@gmail.com>
2021-04-27 10:32:15 -0400

    init

---
+++ src/main/kotlin/com/example/kotlinSpring/HelloWorldController.kt
@@ -0,0 +1,14 @@
+package com.example.kotlinSpring
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/hello")
+class HelloWorldController {
+
+    @GetMapping
+    fun hello(): String = "Hi"
+
+}
